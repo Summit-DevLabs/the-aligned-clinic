ed181bbd1615c0b4610dfccb635c033954faec10
Jesse <jessewalls505@yahoo.com>
2024-11-22 08:48:39 -0500

    created github pages deploy yml file-configured next.config for static exporting

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',  // Enable static exports
+  images: {
+    unoptimized: true, // Required for static export
+  },
+  basePath: '/the-aligned-clinic', // Replace with your repository name
+}
 
 module.exports = nextConfig
